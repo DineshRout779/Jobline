bb275f640f3d835958fd96fde6d8fc7442206662
Dinesh Rout <rddinesh779@gmail.com>
2022-03-31 00:37:29 +0530

    feat: applicant dashboard

--- src/pages/applicant/ApplicantDashboard.jsx
+++ src/pages/applicant/ApplicantDashboard.jsx
@@ -1,7 +1,25 @@
+import { useAppContext } from '../../context/AppContext';
+import JobList from '../../components/JobList';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
 const ApplicantDashboard = () => {
+  const { state } = useAppContext();
+  const navigate = useNavigate();
+  const { isApplicant, user } = state;
+
+  useEffect(() => {
+    user
+      ? isApplicant
+        ? navigate('/applicant')
+        : navigate('/company')
+      : navigate('/applicant/login');
+  }, [user, isApplicant, navigate]);
+
   return (
     <div className='container'>
-      <h1>ApplicantDashboard</h1>
+      <h1>Hello, {user?.name}</h1>
+      <JobList />
     </div>
   );
 };
