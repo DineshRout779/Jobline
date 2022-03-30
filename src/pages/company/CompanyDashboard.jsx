1f3f06fa139a580b55d2dfd976766d9c1b8e6680
Dinesh Rout <rddinesh779@gmail.com>
2022-03-31 00:36:45 +0530

    feat: company dashboard

--- src/pages/company/CompanyDashboard.jsx
+++ src/pages/company/CompanyDashboard.jsx
@@ -1,7 +1,34 @@
+import { AiOutlinePlus } from 'react-icons/ai';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAppContext } from '../../context/AppContext';
+import JobList from '../../components/JobList';
+import { useEffect } from 'react';
+
 const CompanyDashboard = () => {
+  const { state } = useAppContext();
+  const navigate = useNavigate();
+  const { isApplicant, user } = state;
+
+  useEffect(() => {
+    user
+      ? isApplicant
+        ? navigate('/applicant')
+        : navigate('/company')
+      : navigate('/company/login');
+  }, [user, isApplicant, navigate]);
+
   return (
     <div className='container'>
-      <h1>CompanyDashboard</h1>
+      <h1>Welcome {user?.name}</h1>
+      <div className='flex justify-between align-center my-1'>
+        <h3>{isApplicant ? 'All Jobs' : 'Jobs Posted'}</h3>
+        {!isApplicant && (
+          <Link className='btn btn-rev m-0' to='/jobs/add'>
+            <AiOutlinePlus className='svg-icon' /> Post a Job
+          </Link>
+        )}
+      </div>
+      <JobList />
     </div>
   );
 };
