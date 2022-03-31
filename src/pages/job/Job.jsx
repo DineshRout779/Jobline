55095229a265e02a02b3db772078b695abb82f26
Dinesh Rout <rddinesh779@gmail.com>
2022-03-31 18:30:43 +0530

    feat: apply status

--- src/pages/job/Job.jsx
+++ src/pages/job/Job.jsx
@@ -3,17 +3,39 @@ import { useParams } from 'react-router-dom';
 import Loader from '../../components/Loader';
 import axios from 'axios';
 import { api } from '../../backend/api';
+import { useAppContext } from '../../context/AppContext';
+import ButtonSpinner from '../../components/ButtonSpinner';
+import { MdDone } from 'react-icons/md';
 
 const Job = () => {
   const { id } = useParams();
+  const { state } = useAppContext();
+  const { user, isApplicant } = state;
   const [job, setJob] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [show, setShow] = useState(false);
+  const [isAppling, setIsAppling] = useState(false);
+  const [isApplied, setisApplied] = useState(false);
+
+  const handleApplyJob = async () => {
+    setIsAppling(true);
+    try {
+      const res = await axios.put(`${api}/job/${id}/${user._id}`);
+      setIsAppling(false);
+      res.data && setisApplied(true);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
 
   useEffect(() => {
     const fetchJob = async () => {
       try {
         const res = await axios.get(`${api}/job/${id}`);
         setJob(res.data);
+        setisApplied(
+          res.data.applicants.find((applicant) => applicant._id === user._id)
+        );
         setIsLoading(false);
       } catch (error) {
         setIsLoading(false);
@@ -22,7 +44,7 @@ const Job = () => {
     };
 
     fetchJob();
-  }, [id]);
+  }, [id, user._id]);
 
   return (
     <div className='container'>
@@ -41,12 +63,41 @@ const Job = () => {
             <strong>Job-type:</strong> {job.position}
           </p>
           <p>
-            <strong>{job.applicants.length} </strong> Applicants
-          </p>
-          <p>
             <strong>Location:</strong> {job.location}
           </p>
-          <button className='btn m-0'>Apply Now</button>
+          <p>
+            <strong>{job.applicants.length} </strong>{' '}
+            {job.applicantslength > 1 ? 'Applicants' : 'Applicant'}
+          </p>
+          {job.applicants.length > 0 && (
+            <button className='btn btn-rev m-0' onClick={() => setShow(!show)}>
+              Show
+            </button>
+          )}
+          {show && (
+            <ol className='applicant-list'>
+              {job.applicants.length > 0 &&
+                job.applicants.map((applicant, i) => {
+                  return <li key={applicant._id}>{applicant.name}</li>;
+                })}
+            </ol>
+          )}
+
+          {isApplicant && (
+            <button className='btn m-0' onClick={handleApplyJob}>
+              {isApplied ? (
+                <>
+                  Applied <MdDone />
+                </>
+              ) : isAppling ? (
+                <>
+                  Applying <ButtonSpinner />
+                </>
+              ) : (
+                'Apply now'
+              )}
+            </button>
+          )}
         </>
       )}
     </div>
