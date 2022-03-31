89749d7571c4ef80f46da473b64e5113dbd9ddbb
Dinesh Rout <rddinesh779@gmail.com>
2022-03-31 10:50:16 +0530

    style: minor UI changes

---
+++ src/components/ButtonSpinner.jsx
@@ -0,0 +1,8 @@
+const ButtonSpinner = () => {
+  return (
+    <div className='flex justify-center align-center'>
+      <div className='loader loader-button'></div>
+    </div>
+  );
+};
+export default ButtonSpinner;
