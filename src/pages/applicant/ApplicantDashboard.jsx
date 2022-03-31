7a4f2e20a5c6df141bf6ddc949439125a67642d9
Dinesh Rout <rddinesh779@gmail.com>
2022-03-31 10:49:12 +0530

    feat: fetch jobs for applicant

--- src/pages/applicant/ApplicantDashboard.jsx
+++ src/pages/applicant/ApplicantDashboard.jsx
@@ -2,9 +2,11 @@ import { useAppContext } from '../../context/AppContext';
 import JobList from '../../components/JobList';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import axios from 'axios';
+import { api } from '../../backend/api';
 
 const ApplicantDashboard = () => {
-  const { state } = useAppContext();
+  const { state, dispatch } = useAppContext();
   const navigate = useNavigate();
   const { isApplicant, user } = state;
 
@@ -15,6 +17,18 @@ const ApplicantDashboard = () => {
         : navigate('/company')
       : navigate('/applicant/login');
   }, [user, isApplicant, navigate]);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await axios.get(`${api}/job`);
+        res.data && dispatch({ type: 'FETCH_JOBS', payload: res.data });
+      } catch (error) {
+        console.log(error.response.data.error);
+      }
+    };
+    fetchJobs();
+  }, [dispatch]);
 
   return (
     <div className='container'>
